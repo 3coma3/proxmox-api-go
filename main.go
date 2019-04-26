1728d9ad07f2ae05df672bb88df7848906e78063
3coma3 <3coma3@gmail.com>
2019-04-26 14:57:38 -0300

    Switch to test verstion of main.go
    Since the code does things very differently, for example there's no
    login if we want to test a function such as
    (NewConfigQemu/NewConfigLxc)FromJson. Checking out the main.go from
    smoose-test.
    
    The new session.go sports a different version of ResponseJSON that
    returns a value, error (was returning only the value). This triggers
    some more errors in test_proxmox_session.go - commenting out the
    necessary sections so the code builds.

--- main.go
+++ main.go
@@ -1,168 +1,95 @@
 package main
 
 import (
-	"crypto/tls"
-	"encoding/json"
+	"./test"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
-
-	"github.com/Telmate/proxmox-api-go/proxmox"
 )
 
 func main() {
-	var insecure *bool
-	insecure = flag.Bool("insecure", false, "TLS insecure mode")
-	proxmox.Debug = flag.Bool("debug", false, "debug mode")
-	fvmid := flag.Int("vmid", -1, "custom vmid (instead of auto)")
+	defer func() {
+		// exit with an error in case of errors
+		os.Exit(1)
+	}()
+
+	var (
+		err error
+
+		options test.TOptions = test.TOptions{
+			Action:      "",
+			VMid:        0,
+			VMname:      "",
+			APIurl:      os.Getenv("PM_API_URL"),
+			APIuser:     os.Getenv("PM_USER"),
+			APIpass:     os.Getenv("PM_PASS"),
+			APIinsecure: false,
+		}
+
+		fvmid     = flag.Int("vmid", options.VMid, "custom vmid (instead of auto)")
+		fdebug    = flag.Bool("debug", false, "debug mode")
+		finsecure = flag.Bool("insecure", options.APIinsecure, "TLS insecure mode")
+	)
+
 	flag.Parse()
-	tlsconf := &tls.Config{InsecureSkipVerify: true}
-	if !*insecure {
-		tlsconf = nil
+
+	if len(flag.Args()) == 0 {
+		fmt.Printf("Usage: %s [flags] [action] [vmid|vname] [host]\n", os.Args[0])
+		fmt.Printf("\nFlags: \n")
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-	c, _ := proxmox.NewClient(os.Getenv("PM_API_URL"), nil, tlsconf)
-	err := c.Login(os.Getenv("PM_USER"), os.Getenv("PM_PASS"))
-	if err != nil {
-		log.Fatal(err)
+
+	// used by test.DebugMsg
+	test.Debug = *fdebug
+
+	test.DebugMsg("-insecure is " + strconv.FormatBool(*finsecure))
+	test.DebugMsg("-debug is " + strconv.FormatBool(*fdebug))
+	test.DebugMsg("-fvmid is " + strconv.Itoa(*fvmid))
+	for i, v := range flag.Args() {
+		test.DebugMsg("flag.Args()[" + strconv.Itoa(i) + "] is " + v)
 	}
-	vmid := *fvmid
-	if vmid < 0 {
-		if len(flag.Args()) > 1 {
-			vmid, err = strconv.Atoi(flag.Args()[1])
-			if err != nil {
-				vmid = 0
-			}
-		} else if flag.Args()[0] == "idstatus" {
-			vmid = 0
+
+	options.Action = flag.Args()[0]
+
+	// Try to get VMid from the second (non-flag) positional parameter
+	// If that fails (argument is not numeric) or -vmid was specified, just
+	// set the field to whatever *fvmid is and assume the argument is a VMname
+	// This implies that -vmid wins when both it and the positional parameter
+	// are present to set the VMid
+	// The "extra parameter" could be also set here (see below)
+	if len(flag.Args()) > 1 {
+		if options.VMid, err = strconv.Atoi(flag.Args()[1]); *fvmid > 0 || err != nil {
+			options.VMid = *fvmid
+			options.VMname = flag.Args()[1]
+		}
+	} else {
+		options.VMid = *fvmid
+	}
+
+	// for other parameters than vm identifications I refrain to do more checks
+	// to avoid spending time in a CLI until we decide to go with this codebase
+	// copying the arguments array to the options
+	// a good solution might be something like https://github.com/mitchellh/mapstructure
+	// or directly https://github.com/urfave/cli
+	options.Args = flag.Args()
+
+	options.APIinsecure = *finsecure
+
+	// Other validations could be done here, like having extra positional
+	// parameters beyond 3 (action, vmid/vname, node) or having an action that
+	// actually cares about what has been passed
+	// Ignoring these conditions is simpler and won't break things, though
+
+	// the failOnError, fatal, and bools will be later switched to proper
+	// error instance bubbling / wrapping back to main
+	{
+		test.DebugMsg("Running the test: " + options.Action)
+		err := test.Run(&options)
+		test.DebugMsg("The test " + options.Action + " has returned: " + strconv.FormatBool(err))
+		if !err {
+			panic("Test failed")
 		}
 	}
-
-	var jbody interface{}
-	var vmr *proxmox.VmRef
-	switch flag.Args()[0] {
-	case "start":
-		vmr = proxmox.NewVmRef(vmid)
-		jbody, _ = c.StartVm(vmr)
-
-	case "stop":
-
-		vmr = proxmox.NewVmRef(vmid)
-		jbody, _ = c.StopVm(vmr)
-
-	case "destroy":
-		vmr = proxmox.NewVmRef(vmid)
-		jbody, err = c.StopVm(vmr)
-		failError(err)
-		jbody, _ = c.DeleteVm(vmr)
-
-	case "getConfig":
-		vmr = proxmox.NewVmRef(vmid)
-		config, err := proxmox.NewConfigQemuFromApi(vmr, c)
-		failError(err)
-		cj, err := json.MarshalIndent(config, "", "  ")
-		log.Println(string(cj))
-
-	case "createQemu":
-		config, err := proxmox.NewConfigQemuFromJson(os.Stdin)
-		failError(err)
-		vmr = proxmox.NewVmRef(vmid)
-		vmr.SetNode(flag.Args()[2])
-		failError(config.CreateVm(vmr, c))
-		log.Println("Complete")
-
-	case "installQemu":
-		config, err := proxmox.NewConfigQemuFromJson(os.Stdin)
-		failError(err)
-		if vmid > 0 {
-			vmr = proxmox.NewVmRef(vmid)
-		} else {
-			nextid, err := c.GetNextID(0)
-			failError(err)
-			vmr = proxmox.NewVmRef(nextid)
-		}
-		vmr.SetNode(flag.Args()[1])
-		log.Print("Creating node: ")
-		log.Println(vmr)
-		failError(config.CreateVm(vmr, c))
-		_, err = c.StartVm(vmr)
-		failError(err)
-		sshPort, err := proxmox.SshForwardUsernet(vmr, c)
-		failError(err)
-		log.Println("Waiting for CDRom install shutdown (at least 5 minutes)")
-		failError(proxmox.WaitForShutdown(vmr, c))
-		log.Println("Restarting")
-		_, err = c.StartVm(vmr)
-		failError(err)
-		sshPort, err = proxmox.SshForwardUsernet(vmr, c)
-		failError(err)
-		log.Println("SSH Portforward on:" + sshPort)
-		log.Println("Complete")
-
-	case "idstatus":
-		maxid, err := proxmox.MaxVmId(c)
-		failError(err)
-		nextid, err := c.GetNextID(vmid)
-		failError(err)
-		log.Println("---")
-		log.Printf("MaxID: %d\n", maxid)
-		log.Printf("NextID: %d\n", nextid)
-		log.Println("---")
-
-	case "cloneQemu":
-		config, err := proxmox.NewConfigQemuFromJson(os.Stdin)
-		failError(err)
-		log.Println("Looking for template: " + flag.Args()[1])
-		sourceVmr, err := c.GetVmRefByName(flag.Args()[1])
-		failError(err)
-		if sourceVmr == nil {
-			log.Fatal("Can't find template")
-			return
-		}
-		nextid, err := c.GetNextID(0)
-		vmr = proxmox.NewVmRef(nextid)
-		vmr.SetNode(flag.Args()[2])
-		log.Print("Creating node: ")
-		log.Println(vmr)
-		failError(config.CloneVm(sourceVmr, vmr, c))
-		log.Println("Complete")
-
-	case "rollbackQemu":
-		vmr = proxmox.NewVmRef(vmid)
-		jbody, err = c.RollbackQemuVm(vmr, flag.Args()[2])
-		failError(err)
-
-	case "sshforward":
-		vmr = proxmox.NewVmRef(vmid)
-		sshPort, err := proxmox.SshForwardUsernet(vmr, c)
-		failError(err)
-		log.Println("SSH Portforward on:" + sshPort)
-
-	case "sshbackward":
-		vmr = proxmox.NewVmRef(vmid)
-		err = proxmox.RemoveSshForwardUsernet(vmr, c)
-		failError(err)
-		log.Println("SSH Portforward off")
-
-	case "sendstring":
-		vmr = proxmox.NewVmRef(vmid)
-		err = proxmox.SendKeysString(vmr, c, flag.Args()[2])
-		failError(err)
-		log.Println("Keys sent")
-
-	default:
-		fmt.Printf("unknown action, try start|stop vmid")
-	}
-	if jbody != nil {
-		log.Println(jbody)
-	}
-	//log.Println(vmr)
-}
-
-func failError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-	return
 }
