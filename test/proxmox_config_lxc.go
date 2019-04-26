2370617a1fd4a964fe60392fc317b12dce5a68b8
3coma3 <3coma3@gmail.com>
2019-04-26 14:30:51 -0300

    Change testActions string keys for config_lxc

--- test/proxmox_config_lxc.go
+++ test/proxmox_config_lxc.go
@@ -8,10 +8,10 @@ import (
 )
 
 func init() {
-	testActions["configqemu_createvm"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_createvm"] = func(options *TOptions) (response interface{}, err error) {
 		client, vmr := newClientAndVmr(options)
 
-		config, err := proxmox.NewConfigQemuFromJson(os.Stdin)
+		config, err := proxmox.NewConfiglxcFromJson(os.Stdin)
 		failOnError(err)
 
 		vmr.SetNode(options.Args[1])
@@ -20,12 +20,12 @@ func init() {
 	}
 
 	// simple method
-	testActions["configqemu_hascloudinit"] = errNotImplemented
+	testActions["configlxc_hascloudinit"] = errNotImplemented
 
-	testActions["configqemu_clonevm"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_clonevm"] = func(options *TOptions) (response interface{}, err error) {
 		client, vmr := newClientAndVmr(options)
 
-		config, err := proxmox.NewConfigQemuFromJson(os.Stdin)
+		config, err := proxmox.NewConfigLxcFromJson(os.Stdin)
 		failOnError(err)
 
 		DebugMsg("Looking for template: " + options.VMname)
@@ -42,10 +42,10 @@ func init() {
 		return nil, err
 	}
 
-	testActions["configqemu_updateconfig"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_updateconfig"] = func(options *TOptions) (response interface{}, err error) {
 		client, vmr := newClientAndVmr(options)
 
-		config, err := proxmox.NewConfigQemuFromJson(os.Stdin)
+		config, err := proxmox.NewConfigLxcFromJson(os.Stdin)
 		failOnError(err)
 
 		vminfo, err := client.GetVmInfo(vmr)
@@ -57,16 +57,16 @@ func init() {
 		return nil, err
 	}
 
-	testActions["configqemu_newconfigqemufromjson"] = func(options *TOptions) (response interface{}, err error) {
-		return proxmox.NewConfigQemuFromJson(os.Stdin)
+	testActions["configlxc_newconfiglxcfromjson"] = func(options *TOptions) (response interface{}, err error) {
+		return proxmox.NewConfigLxcFromJson(os.Stdin)
 	}
 
-	testActions["configqemu_newconfigqemufromapi"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_newconfiglxcfromapi"] = func(options *TOptions) (response interface{}, err error) {
 		client, vmr := newClientAndVmr(options)
-		return proxmox.NewConfigQemuFromApi(vmr, client)
+		return proxmox.NewConfigLxcFromApi(vmr, client)
 	}
 
-	testActions["configqemu_waitforshutdown"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_waitforshutdown"] = func(options *TOptions) (response interface{}, err error) {
 		client, vmr := newClientAndVmr(options)
 
 		// remember to use this to shutdown asynchronously
@@ -75,66 +75,66 @@ func init() {
 		return nil, proxmox.WaitForShutdown(vmr, client)
 	}
 
-	testActions["configqemu_sshforwardusernet"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_sshforwardusernet"] = func(options *TOptions) (response interface{}, err error) {
 		client, vmr := newClientAndVmr(options)
 		return proxmox.SshForwardUsernet(vmr, client)
 	}
 
-	testActions["configqemu_removesshforwardusernet"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_removesshforwardusernet"] = func(options *TOptions) (response interface{}, err error) {
 		client, vmr := newClientAndVmr(options)
 		return nil, proxmox.RemoveSshForwardUsernet(vmr, client)
 	}
 
-	testActions["configqemu_maxvmid"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_maxvmid"] = func(options *TOptions) (response interface{}, err error) {
 		client, _ := newClientAndVmr(options)
 		return proxmox.MaxVmId(client)
 	}
 
-	testActions["configqemu_sendkeysstring"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_sendkeysstring"] = func(options *TOptions) (response interface{}, err error) {
 		client, vmr := newClientAndVmr(options)
 		return nil, proxmox.SendKeysString(vmr, client, options.Args[1])
 	}
 
-	testActions["configqemu_createqemunetworksparams"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_createlxcnetworksparams"] = func(options *TOptions) (response interface{}, err error) {
 		// only the json for the network is needed on stdin
-		inputparams := proxmox.QemuDevice{}
+		inputparams := proxmox.LxcDevice{}
 
 		// put whatever json is on stdin into a map[string]interface{}
 		failOnError(json.NewDecoder(os.Stdin).Decode(&inputparams))
 
-		// put the map as QemuNetworks[0] as if it were built by
-		// NewConfigQemuFromJson
-		config := &proxmox.ConfigQemu{
-			QemuNetworks: proxmox.QemuDevices{0: inputparams},
+		// put the map as LxcNetworks[0] as if it were built by
+		// NewConfigLxcFromJson
+		config := &proxmox.ConfigLxc{
+			LxcNetworks: proxmox.LxcDevices{0: inputparams},
 		}
 
 		// so now this method can build the PVEAPI-compatible "premap"
 		// this is a map of keys to config items, each config item will have
 		// a device name and a configuration with two levels of subelements
 		// this method rewrites heavily the input parameters
-		premap := proxmox.QemuDevice{}
-		return premap, config.CreateQemuNetworksParams(options.VMid, premap)
+		premap := proxmox.LxcDevice{}
+		return premap, config.CreateLxcNetworksParams(options.VMid, premap)
 	}
 
-	testActions["configqemu_createqemudisksparams"] = func(options *TOptions) (response interface{}, err error) {
+	testActions["configlxc_createlxcdisksparams"] = func(options *TOptions) (response interface{}, err error) {
 		// only the json for the network interfaces is needed on stdin
-		inputparams := proxmox.QemuDevice{}
+		inputparams := proxmox.LxcDevice{}
 
 		// put whatever json is on stdin into a map[string]interface{}
 		failOnError(json.NewDecoder(os.Stdin).Decode(&inputparams))
 
-		// put the map as QemuDisks[0] as if it were built by
-		// NewConfigQemuFromJson
-		config := &proxmox.ConfigQemu{
-			QemuDisks: proxmox.QemuDevices{0: inputparams},
+		// put the map as LxcDisks[0] as if it were built by
+		// NewConfigLxcFromJson
+		config := &proxmox.ConfigLxc{
+			LxcDisks: proxmox.LxcDevices{0: inputparams},
 		}
 
 		// so now this method can build the PVEAPI-compatible "premap"
 		// this is a map of keys to config items, each config item will have
 		// a device name and a configuration with two levels of subelements
 		// this method rewrites heavily the input parameters
-		premap := proxmox.QemuDevice{}
-		return premap, config.CreateQemuDisksParams(options.VMid, premap, false)
+		premap := proxmox.LxcDevice{}
+		return premap, config.CreateLxcDisksParams(options.VMid, premap, false)
 	}
 
 }
